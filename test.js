83c00d22e96429368ef4df2752e27ed49f7d9ff9
Sindre Sorhus <sindresorhus@gmail.com>
2014-04-07 12:56:34 +0200

    minor tweaks

--- test.js
+++ test.js
@@ -1,33 +1,30 @@
-/*global describe, it, beforeEach */
 'use strict';
 var assert = require('assert');
 
-describe('hasColor', function () {
-	beforeEach(function () {
-		// clear the cache of the tested module
-		delete require.cache[require.resolve('./has-color')];
-		process.stdout.isTTY = true;
-		process.argv = [];
-		process.env = {};
-	});
+beforeEach(function () {
+	// clear the cache of the tested module
+	delete require.cache[require.resolve('./index')];
+	process.stdout.isTTY = true;
+	process.argv = [];
+	process.env = {};
+});
 
-	it('should return false if not TTY', function () {
-		process.stdout.isTTY = false;
-		assert.equal(require('./has-color'), false);
-	});
+it('should return false if not TTY', function () {
+	process.stdout.isTTY = false;
+	assert.equal(require('./index'), false);
+});
 
-	it('should return false if --no-color flag is used', function () {
-		process.argv = ['--no-color'];
-		assert.equal(require('./has-color'), false);
-	});
+it('should return false if --no-color flag is used', function () {
+	process.argv = ['--no-color'];
+	assert.equal(require('./index'), false);
+});
 
-	it('should return true if --color flag is used', function () {
-		process.argv = ['--color'];
-		assert.equal(require('./has-color'), true);
-	});
+it('should return true if --color flag is used', function () {
+	process.argv = ['--color'];
+	assert.equal(require('./index'), true);
+});
 
-	it('should return true if `COLORTERM` is in env', function () {
-		process.env.COLORTERM = true;
-		assert.equal(require('./has-color'), true);
-	});
+it('should return true if `COLORTERM` is in env', function () {
+	process.env.COLORTERM = true;
+	assert.equal(require('./index'), true);
 });
