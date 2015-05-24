efabe9490dd8ddfd45fa28138aac8ba9bd1ee280
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-05-24 18:35:18 +0200

    Use `meow` in the CLI

--- cli.js
+++ cli.js
@@ -1,29 +1,15 @@
 #!/usr/bin/env node
 'use strict';
-var pkg = require('./package.json');
+var meow = require('meow');
 var supportsColor = require('./');
-var argv = process.argv.slice(2);
 
-function help() {
-	console.log([
+meow({
+	help: [
+		'Usage',
+		'  supports-color',
 		'',
-		'  ' + pkg.description,
-		'',
-		'  Usage',
-		'    supports-color',
-		'',
-		'  Exits with code 0 if color is supported and 1 if not'
-	].join('\n'));
-}
-
-if (argv.indexOf('--help') !== -1) {
-	help();
-	return;
-}
-
-if (argv.indexOf('--version') !== -1) {
-	console.log(pkg.version);
-	return;
-}
+		'Exits with code 0 if color is supported and 1 if not'
+	].join('\n')
+});
 
 process.exit(supportsColor ? 0 : 1);
