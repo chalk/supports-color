cc4a782325b54737269cc0ea1282e259f868cb90
Sindre Sorhus <sindresorhus@gmail.com>
2017-01-23 02:13:14 +0700

    Switch tests to AVA

--- test.js
+++ test.js
@@ -1,186 +1,185 @@
-'use strict';
-const assert = require('assert');
-const requireUncached = require('require-uncached');
+import {serial as test} from 'ava';
+import requireUncached from 'require-uncached';
 
-beforeEach(() => {
+test.beforeEach(() => {
 	process.stdout.isTTY = true;
 	process.argv = [];
 	process.env = {};
 });
 
-it('should return true if `FORCE_COLOR` is in env', () => {
+test('return true if `FORCE_COLOR` is in env', t => {
 	process.env.FORCE_COLOR = true;
-	const result = requireUncached('./');
-	assert.equal(Boolean(result), true);
-	assert.equal(result.level, 1);
+	const result = requireUncached('.');
+	t.truthy(result);
+	t.is(result.level, 1);
 });
 
-it('should return true if `FORCE_COLOR` is in env, but honor 256', () => {
+test('return true if `FORCE_COLOR` is in env, but honor 256', t => {
 	process.argv = ['--color=256'];
 	process.env.FORCE_COLOR = true;
-	const result = requireUncached('./');
-	assert.equal(Boolean(result), true);
-	assert.equal(result.level, 2);
+	const result = requireUncached('.');
+	t.truthy(result);
+	t.is(result.level, 2);
 });
 
-it('should return true if `FORCE_COLOR` is in env, but honor 256', () => {
+test('return true if `FORCE_COLOR` is in env, but honor 256', t => {
 	process.argv = ['--color=256'];
 	process.env.FORCE_COLOR = '1';
-	const result = requireUncached('./');
-	assert.equal(Boolean(result), true);
-	assert.equal(result.level, 2);
+	const result = requireUncached('.');
+	t.truthy(result);
+	t.is(result.level, 2);
 });
 
-it('should return false if `FORCE_COLOR` is in env and is 0', () => {
+test('return false if `FORCE_COLOR` is in env and is 0', t => {
 	process.env.FORCE_COLOR = '0';
-	const result = requireUncached('./');
-	assert.equal(Boolean(result), false);
+	const result = requireUncached('.');
+	t.false(result);
 });
 
-it('should return false if not TTY', () => {
+test('return false if not TTY', t => {
 	process.stdout.isTTY = false;
-	const result = requireUncached('./');
-	assert.equal(Boolean(result), false);
+	const result = requireUncached('.');
+	t.false(result);
 });
 
-it('should return false if --no-color flag is used', () => {
+test('return false if --no-color flag is used', t => {
 	process.env = {TERM: 'xterm-256color'};
 	process.argv = ['--no-color'];
-	const result = requireUncached('./');
-	assert.equal(Boolean(result), false);
+	const result = requireUncached('.');
+	t.false(result);
 });
 
-it('should return false if --no-colors flag is used', () => {
+test('return false if --no-colors flag is used', t => {
 	process.env = {TERM: 'xterm-256color'};
 	process.argv = ['--no-colors'];
-	const result = requireUncached('./');
-	assert.equal(Boolean(result), false);
+	const result = requireUncached('.');
+	t.false(result);
 });
 
-it('should return true if --color flag is used', () => {
+test('return true if --color flag is used', t => {
 	process.argv = ['--color'];
-	const result = requireUncached('./');
-	assert.equal(Boolean(result), true);
+	const result = requireUncached('.');
+	t.truthy(result);
 });
 
-it('should return true if --colors flag is used', () => {
+test('return true if --colors flag is used', t => {
 	process.argv = ['--colors'];
-	const result = requireUncached('./');
-	assert.equal(Boolean(result), true);
+	const result = requireUncached('.');
+	t.truthy(result);
 });
 
-it('should return true if `COLORTERM` is in env', () => {
+test('return true if `COLORTERM` is in env', t => {
 	process.env.COLORTERM = true;
-	const result = requireUncached('./');
-	assert.equal(Boolean(result), true);
+	const result = requireUncached('.');
+	t.truthy(result);
 });
 
-it('should support `--color=true` flag', () => {
+test('support `--color=true` flag', t => {
 	process.argv = ['--color=true'];
-	const result = requireUncached('./');
-	assert.equal(Boolean(result), true);
+	const result = requireUncached('.');
+	t.truthy(result);
 });
 
-it('should support `--color=always` flag', () => {
+test('support `--color=always` flag', t => {
 	process.argv = ['--color=always'];
-	const result = requireUncached('./');
-	assert.equal(Boolean(result), true);
+	const result = requireUncached('.');
+	t.truthy(result);
 });
 
-it('should support `--color=false` flag', () => {
+test('support `--color=false` flag', t => {
 	process.env = {TERM: 'xterm-256color'};
 	process.argv = ['--color=false'];
-	const result = requireUncached('./');
-	assert.equal(Boolean(result), false);
+	const result = requireUncached('.');
+	t.false(result);
 });
 
-it('should support `--color=256` flag', () => {
+test('support `--color=256` flag', t => {
 	process.argv = ['--color=256'];
-	const result = requireUncached('./');
-	assert.equal(Boolean(result), true);
+	const result = requireUncached('.');
+	t.truthy(result);
 });
 
-it('level should be 2 if `--color=256` flag is used', () => {
+test('level should be 2 if `--color=256` flag is used', t => {
 	process.argv = ['--color=256'];
-	const result = requireUncached('./');
-	assert.equal(result.level, 2);
-	assert.equal(result.has256, true);
+	const result = requireUncached('.');
+	t.is(result.level, 2);
+	t.true(result.has256);
 });
 
-it('should support `--color=16m` flag', () => {
+test('support `--color=16m` flag', t => {
 	process.argv = ['--color=16m'];
-	const result = requireUncached('./');
-	assert.equal(Boolean(result), true);
+	const result = requireUncached('.');
+	t.truthy(result);
 });
 
-it('should support `--color=full` flag', () => {
+test('support `--color=full` flag', t => {
 	process.argv = ['--color=full'];
-	const result = requireUncached('./');
-	assert.equal(Boolean(result), true);
+	const result = requireUncached('.');
+	t.truthy(result);
 });
 
-it('should support `--color=truecolor` flag', () => {
+test('support `--color=truecolor` flag', t => {
 	process.argv = ['--color=truecolor'];
-	const result = requireUncached('./');
-	assert.equal(Boolean(result), true);
+	const result = requireUncached('.');
+	t.truthy(result);
 });
 
-it('level should be 3 if `--color=16m` flag is used', () => {
+test('level should be 3 if `--color=16m` flag is used', t => {
 	process.argv = ['--color=16m'];
-	const result = requireUncached('./');
-	assert.equal(result.level, 3);
-	assert.equal(result.has256, true);
-	assert.equal(result.has16m, true);
+	const result = requireUncached('.');
+	t.is(result.level, 3);
+	t.true(result.has256);
+	t.true(result.has16m);
 });
 
-it('should ignore post-terminator flags', () => {
+test('ignore post-terminator flags', t => {
 	process.argv = ['--color', '--', '--no-color'];
-	const result = requireUncached('./');
-	assert.equal(Boolean(result), true);
+	const result = requireUncached('.');
+	t.truthy(result);
 });
 
-it('should allow tests of the properties on false', () => {
+test('allow tests of the properties on false', t => {
 	process.env = {TERM: 'xterm-256color'};
 	process.argv = ['--no-color'];
-	const result = requireUncached('./');
-	assert.equal(Boolean(result.hasBasic), false);
-	assert.equal(Boolean(result.has256), false);
-	assert.equal(Boolean(result.has16m), false);
-	assert.equal(result.level > 0, false);
+	const result = requireUncached('.');
+	t.is(result.hasBasic, undefined);
+	t.is(result.has256, undefined);
+	t.is(result.has16m, undefined);
+	t.false(result.level > 0);
 });
 
-it('should return false if `CI` is in env', () => {
+test('return false if `CI` is in env', t => {
 	process.env.CI = 'AppVeyor';
-	const result = requireUncached('./');
-	assert.equal(Boolean(result), false);
+	const result = requireUncached('.');
+	t.false(result);
 });
 
-it('should return true if `TRAVIS` is in env', () => {
+test('return true if `TRAVIS` is in env', t => {
 	process.env = {CI: 'Travis', TRAVIS: '1'};
-	const result = requireUncached('./');
-	assert.equal(Boolean(result), true);
+	const result = requireUncached('.');
+	t.truthy(result);
 });
 
-it('should return false if `TEAMCITY_VERSION` is in env and is < 9.1', () => {
+test('return false if `TEAMCITY_VERSION` is in env and is < 9.1', t => {
 	process.env.TEAMCITY_VERSION = '9.0.5 (build 32523)';
-	const result = requireUncached('./');
-	assert.equal(Boolean(result), false);
+	const result = requireUncached('.');
+	t.false(result);
 });
 
-it('should return level 1 if `TEAMCITY_VERSION` is in env and is >= 9.1', () => {
+test('return level 1 if `TEAMCITY_VERSION` is in env and is >= 9.1', t => {
 	process.env.TEAMCITY_VERSION = '9.1.0 (build 32523)';
-	const result = requireUncached('./');
-	assert.equal(result.level, 1);
+	const result = requireUncached('.');
+	t.is(result.level, 1);
 });
 
-it('should prefer level 2/xterm over COLORTERM', () => {
+test('prefer level 2/xterm over COLORTERM', t => {
 	process.env = {COLORTERM: '1', TERM: 'xterm-256color'};
-	const result = requireUncached('./');
-	assert.equal(result.level, 2);
+	const result = requireUncached('.');
+	t.is(result.level, 2);
 });
 
-it('should support screen-256color', () => {
+test('support screen-256color', t => {
 	process.env = {TERM: 'screen-256color'};
-	const result = requireUncached('./');
-	assert.equal(result.level, 2);
+	const result = requireUncached('.');
+	t.is(result.level, 2);
 });
