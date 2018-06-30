db00aef56fd75bdfcc74a9eef02908d431da9550
bluelovers <codelovers@users.sourceforge.net>
2018-06-30 08:08:44 +0800

    .

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ if (hasFlag('no-color') ||
 	forceColor = true;
 }
 if ('FORCE_COLOR' in env) {
-	let s = String(env.FORCE_COLOR).toLowerCase();
-	forceColor = s === 'true' || env.FORCE_COLOR.length === 0 || env.FORCE_COLOR && s !== 'false' && parseInt(env.FORCE_COLOR, 10) !== 0;
+	const v = require('env-bool').envBool(env.FORCE_COLOR, true);
+	forceColor = v && v > -1;
 }
 
 function translateLevel(level) {
