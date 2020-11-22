3eaa066a4593804d10b1dcbf67ef0c7a6e48fade
Richie Bendall <richiebendall@gmail.com>
2020-11-22 23:17:54 +1300

    Detect Google Chrome

--- browser.js
+++ browser.js
@@ -1,5 +1,23 @@
 'use strict';
+
+function getChromeVersion() {
+	const matches = /Chrom(e|ium)\/(?<chromeVersion>\d+)\./.exec(navigator.userAgent);
+
+	if (!matches) {
+		return undefined;
+	}
+
+	return Number.parseInt(matches.groups.chromeVersion, 10);
+}
+
+const colorSupport = getChromeVersion() > 69 ? {
+	level: 1,
+	hasBasic: true,
+	has256: false,
+	has16m: false
+} : false
+
 module.exports = {
-	stdout: false,
-	stderr: false
+	stdout: colorSupport,
+	stderr: colorSupport
 };
