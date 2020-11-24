0f201684822c3dc2101b5f972fdb4e960bd71221
Richie Bendall <richiebendall@gmail.com>
2020-11-24 16:48:51 +1300

    Update browser.js

--- browser.js
+++ browser.js
@@ -2,7 +2,7 @@
 'use strict';
 
 function getChromeVersion() {
-	const matches = /(Chrome|Chromium)\/(?<chromeVersion>\d+)\./.exec(navigator && navigator.userAgent);
+	const matches = /(Chrome|Chromium)\/(?<chromeVersion>\d+)\./.exec(navigator.userAgent);
 
 	if (!matches) {
 		return;
