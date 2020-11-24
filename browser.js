9d831ab529bb7e2f420499e0f151e952e1944d43
Richie Bendall <richiebendall@gmail.com>
2020-11-24 16:48:35 +1300

    Update browser.js
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- browser.js
+++ browser.js
@@ -2,7 +2,7 @@
 'use strict';
 
 function getChromeVersion() {
-	const matches = /Chrom(e|ium)\/(?<chromeVersion>\d+)\./.exec(navigator && navigator.userAgent);
+	const matches = /(Chrome|Chromium)\/(?<chromeVersion>\d+)\./.exec(navigator && navigator.userAgent);
 
 	if (!matches) {
 		return;
