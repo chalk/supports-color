c1f9b790c23e5668e5393a2bd4f72d2dfbd640a1
April Arcus <april.arcus@gmail.com>
2022-08-28 23:37:23 -0700

    support truecolor in kitty

--- index.js
+++ index.js
@@ -125,6 +125,10 @@ function _supportsColor(haveStream, {streamIsTTY, sniffFlags = true} = {}) {
 		return 3;
 	}
 
+	if (env.TERM === 'xterm-kitty') {
+		return 3;
+	}
+
 	if ('TERM_PROGRAM' in env) {
 		const version = Number.parseInt((env.TERM_PROGRAM_VERSION || '').split('.')[0], 10);
 
